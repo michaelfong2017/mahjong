f8c2c8e1e9e2c60c6c5da67e4b7309a46a4a09f5
Michael Fong <michaelfong2017@gmail.com>
2020-02-29 07:57:12 +0800

    Added Main.DebugLog. Now very similar for both .NET and unity.

--- C#/Reinforcement_learning/Reinforcement_Learning/Main.cs
+++ C#/Reinforcement_learning/Reinforcement_Learning/Main.cs
@@ -10,8 +10,13 @@ namespace Reinforcement_Learning
         public static void Start()
         {
             Reinforcement_Learning.Tile tile0 = new Reinforcement_Learning.Tile("04");
-            Console.WriteLine(tile0);
-            Console.WriteLine(tile0.tile_pattern);
+            Main.DebugLog(tile0);
+            Main.DebugLog(tile0.tile_pattern);
+        }
+
+        public static void DebugLog(object message)
+        {
+            Console.WriteLine(message);
         }
     }
 }
